ed6ed09d9b91b8b4286f0e250da3e113dfd7d5a9
Zeljko Stankovic <zeljko.stankovic@bison-group.com>
2022-06-20 18:17:09 +0200

    added bottomNav with 3 Main Screen

--- app/src/main/java/com/zeljkostankovic/safbox_app/MainActivity.java
+++ app/src/main/java/com/zeljkostankovic/safbox_app/MainActivity.java
@@ -1,14 +1,28 @@
 package com.zeljkostankovic.safbox_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+
     }
 }
