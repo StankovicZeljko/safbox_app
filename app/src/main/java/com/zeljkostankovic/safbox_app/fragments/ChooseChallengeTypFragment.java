6e0229cc5ca79516af68c847d143f9a6bfb47491
Zeljko Stankovic <zeljko.stankovic@bison-group.com>
2022-07-07 16:58:13 +0200

    added all fragments for new Challenge

--- app/src/main/java/com/zeljkostankovic/safbox_app/fragments/ChooseChallengeTypFragment.java
+++ app/src/main/java/com/zeljkostankovic/safbox_app/fragments/ChooseChallengeTypFragment.java
@@ -2,65 +2,60 @@ package com.zeljkostankovic.safbox_app.fragments;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.zeljkostankovic.safbox_app.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChooseChallengeTypFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ChooseChallengeTypFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ChooseChallengeTypFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChooseChallengeTypFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChooseChallengeTypFragment newInstance(String param1, String param2) {
-        ChooseChallengeTypFragment fragment = new ChooseChallengeTypFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_challenge_typ, container, false);
+        View view= inflater.inflate(R.layout.fragment_choose_challenge_typ, container, false);
+
+        ImageButton imageButton = view.findViewById(R.id.backBTN);
+
+        CardView cardViewTime = view.findViewById(R.id.cardViewTime);
+
+        CardView cardViewCode = view.findViewById(R.id.cardViewCode);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Navigation.findNavController(view).navigate(R.id.action_chooseChallengeTypFragment_to_challengeFragment);
+
+            }
+        });
+
+
+        cardViewTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_chooseChallengeTypFragment_to_newTimeChallengeFragment);
+            }
+        });
+
+
+        cardViewCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_chooseChallengeTypFragment_to_newCodeChallengeFragment);
+            }
+        });
+
+        return view;
+
     }
+
 }
